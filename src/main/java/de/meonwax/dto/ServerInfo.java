88ec8cea245855bd2bb43eaf864c0b63e18558a9
meonwax <meonwax@meonwax.de>
2016-03-30 17:26:01 +0200

    Simplified and extended creation of ServerInfo

--- src/main/java/de/meonwax/dto/ServerInfo.java
+++ src/main/java/de/meonwax/dto/ServerInfo.java
@@ -3,17 +3,33 @@ package de.meonwax.dto;
 import java.io.Serializable;
 import java.time.ZonedDateTime;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
 public class ServerInfo implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    @Value("${predictr.version}")
     private String version;
 
+    @Value("${predictr.title}")
     private String title;
 
-    public ServerInfo(String version, String title) {
-        this.version = version;
-        this.title = title;
+    @Value("${predictr.adminEmail}")
+    private String adminEmail;
+
+    @Value("${predictr.points.result}")
+    private int pointsResult;
+
+    @Value("${predictr.points.tendency}")
+    private int pointsTendency;
+
+    @Value("${predictr.points.tendencySpread}")
+    private int pointsTendencySpread;
+
+    public ServerInfo() {
     }
 
     public ZonedDateTime getTime() {
@@ -26,5 +42,21 @@ public class ServerInfo implements Serializable {
 
     public String getTitle() {
         return title;
+    }
+
+    public String getAdminEmail() {
+        return adminEmail;
+    }
+
+    public int getPointsResult() {
+        return pointsResult;
+    }
+
+    public int getPointsTendency() {
+        return pointsTendency;
+    }
+
+    public int getPointsTendencySpread() {
+        return pointsTendencySpread;
     }
 }
--- src/main/java/de/meonwax/web/InfoController.java
+++ src/main/java/de/meonwax/web/InfoController.java
@@ -1,9 +1,6 @@
 package de.meonwax.web;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,13 +12,11 @@ import de.meonwax.dto.ServerInfo;
 @RequestMapping("api")
 public class InfoController {
 
-    private final Logger log = LoggerFactory.getLogger(InfoController.class);
-
     @Autowired
-    private Environment env;
+    private ServerInfo serverInfo;
 
     @RequestMapping(value = "/info", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public ServerInfo get() {
-        return new ServerInfo(env.getProperty("predictr.version", "unknown"), env.getProperty("predictr.title", "Predictr"));
+        return serverInfo;
     }
 }
