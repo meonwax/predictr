cf4d57c810090ae624f18e61379d0d405b353813
meonwax <meonwax@meonwax.de>
2016-03-31 11:24:52 +0200

    Externalized routes config

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -11,53 +11,7 @@ angular.module('predictrApp', ['ngRoute', 'pascalprecht.translate', 'ngCookies',
     };
   })
   .config(function($routeProvider, $translateProvider) {
-
-    /*
-    * Routes
-    */
-    $routeProvider
-      .when('/', {
-        templateUrl: 'templates/home.html',
-        controller: 'HomeCtrl',
-        activeTab: 'home'
-      })
-      .when('/bets', {
-        templateUrl: 'templates/bets.html',
-        controller: 'BetsCtrl',
-        activeTab: 'bets'
-      })
-      .when('/ladder', {
-        templateUrl: 'templates/ladder.html',
-        controller: 'LadderCtrl',
-        activeTab: 'ladder'
-      })
-      .when('/questions', {
-        templateUrl: 'templates/questions.html',
-        controller: 'QuestionsCtrl',
-        activeTab: 'questions'
-      })
-      .when('/rules', {
-        templateUrl: 'templates/rules.html',
-        controller: 'RulesCtrl',
-        activeTab: 'rules'
-      })
-      .when('/shoutbox', {
-        templateUrl: 'templates/shoutbox.html',
-        controller: 'ShoutboxCtrl',
-        activeTab: 'shoutbox'
-      })
-      .when('/admin', {
-        templateUrl: 'templates/admin.html',
-        controller: 'AdminCtrl',
-        activeTab: 'admin'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-
-    /*
-    * I18n
-    */
+    // I18n
     $translateProvider
       .useStaticFilesLoader({
         prefix: 'data/i18n-',
---
+++ src/main/webapp/js/routes.js
@@ -0,0 +1,44 @@
+'use strict';
+
+angular.module('predictrApp')
+  .config(function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'templates/home.html',
+        controller: 'HomeCtrl',
+        activeTab: 'home'
+      })
+      .when('/bets', {
+        templateUrl: 'templates/bets.html',
+        controller: 'BetsCtrl',
+        activeTab: 'bets'
+      })
+      .when('/ladder', {
+        templateUrl: 'templates/ladder.html',
+        controller: 'LadderCtrl',
+        activeTab: 'ladder'
+      })
+      .when('/questions', {
+        templateUrl: 'templates/questions.html',
+        controller: 'QuestionsCtrl',
+        activeTab: 'questions'
+      })
+      .when('/rules', {
+        templateUrl: 'templates/rules.html',
+        controller: 'RulesCtrl',
+        activeTab: 'rules'
+      })
+      .when('/shoutbox', {
+        templateUrl: 'templates/shoutbox.html',
+        controller: 'ShoutboxCtrl',
+        activeTab: 'shoutbox'
+      })
+      .when('/admin', {
+        templateUrl: 'templates/admin.html',
+        controller: 'AdminCtrl',
+        activeTab: 'admin'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
