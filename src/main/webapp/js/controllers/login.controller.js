cb05c490b1d3a4864516179ef94bfd0e33cd0eb8
meonwax <meonwax@meonwax.de>
2016-04-20 14:49:16 +0200

    Add focus to email field in login page

--- src/main/webapp/js/controllers/login.controller.js
+++ src/main/webapp/js/controllers/login.controller.js
@@ -4,13 +4,16 @@ angular.module('predictrApp')
   .controller('LoginCtrl', function($scope, $location, Authentication) {
 
     $scope.credentials = {};
+    $scope.focusInput = true;
 
     $scope.login = function() {
+      $scope.focusInput = false;
       Authentication.login($scope.credentials)
         .then(function() {
           $scope.error = false;
           $location.path('');
         }, function(error) {
+          $scope.focusInput = true;
           $scope.error = true;
         })
     };
