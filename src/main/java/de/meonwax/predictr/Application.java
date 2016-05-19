60ee186e97109bc793675c4ad500905e9f84f60b
meonwax <meonwax@meonwax.de>
2016-05-19 15:51:08 +0200

    Create a deployable and executable war file with maven

--- src/main/java/de/meonwax/predictr/Application.java
+++ src/main/java/de/meonwax/predictr/Application.java
@@ -2,12 +2,19 @@ package de.meonwax.predictr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
 @EntityScan(basePackageClasses = { Application.class, Jsr310JpaConverters.class })
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
