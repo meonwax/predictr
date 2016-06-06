5ff462bb21e5059b840e730fbd2549d910c27b10
meonwax <meonwax@meonwax.de>
2016-06-06 23:10:54 +0200

    Now using localStorage for storing auth token

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -43,7 +43,7 @@ angular.module('predictrApp', ['ngRoute', 'pascalprecht.translate', 'angularMome
     // Local storage
     localStorageServiceProvider
       .setPrefix('predictr')
-      .setStorageType('sessionStorage');
+      .setStorageType('localStorage');
 
     //Toastr
     angular.extend(toastrConfig, {
