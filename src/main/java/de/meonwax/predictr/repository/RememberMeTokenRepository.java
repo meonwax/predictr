07b9ed430c68db51b0e19e688746f2e9c99ec1f9
meonwax <meonwax@meonwax.de>
2016-06-11 22:43:53 +0200

    Fix for defective deletion of RememberMeTokens

--- src/main/java/de/meonwax/predictr/repository/RememberMeTokenRepository.java
+++ src/main/java/de/meonwax/predictr/repository/RememberMeTokenRepository.java
@@ -1,7 +1,5 @@
 package de.meonwax.predictr.repository;
 
-import java.util.Set;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import de.meonwax.predictr.domain.RememberMeToken;
@@ -9,5 +7,5 @@ import de.meonwax.predictr.domain.User;
 
 public interface RememberMeTokenRepository extends JpaRepository<RememberMeToken, String> {
 
-    public Set<RememberMeToken> findAllByUser(User user);
+    public Long deleteByUser(User user);
 }
--- src/main/java/de/meonwax/predictr/security/PersistentTokenRepositoryImpl.java
+++ src/main/java/de/meonwax/predictr/security/PersistentTokenRepositoryImpl.java
@@ -3,7 +3,8 @@ package de.meonwax.predictr.security;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.util.Date;
-import java.util.Set;
+
+import javax.transaction.Transactional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,18 +18,20 @@ import de.meonwax.predictr.repository.RememberMeTokenRepository;
 import de.meonwax.predictr.repository.UserRepository;
 
 @Component
+@Transactional
 public class PersistentTokenRepositoryImpl implements PersistentTokenRepository {
 
     private final Logger log = LoggerFactory.getLogger(PersistentTokenRepositoryImpl.class);
 
     @Autowired
-    RememberMeTokenRepository rememberMeTokenRepository;
+    private RememberMeTokenRepository rememberMeTokenRepository;
 
     @Autowired
-    UserRepository userRepository;
+    private UserRepository userRepository;
 
     @Override
     public void createNewToken(PersistentRememberMeToken token) {
+        log.debug("Creating new RememberMeToken for user " + token.getUsername());
         RememberMeToken rememberMeToken = new RememberMeToken();
         rememberMeToken.setUser(userRepository.findOneByEmailIgnoringCase(token.getUsername()));
         rememberMeToken.setSeries(token.getSeries());
@@ -39,6 +42,7 @@ public class PersistentTokenRepositoryImpl implements PersistentTokenRepository 
 
     @Override
     public void updateToken(String seriesId, String tokenValue, Date lastUsed) {
+        log.debug("Updating RememberMeToken for series " + seriesId);
         RememberMeToken rememberMeToken = rememberMeTokenRepository.findOne(seriesId);
         rememberMeToken.setValue(tokenValue);
         rememberMeToken.setDate(ZonedDateTime.ofInstant(lastUsed.toInstant(), ZoneId.systemDefault()));
@@ -47,19 +51,19 @@ public class PersistentTokenRepositoryImpl implements PersistentTokenRepository 
 
     @Override
     public PersistentRememberMeToken getTokenForSeries(String seriesId) {
+        log.debug("Try to find RememberMeToken for series " + seriesId);
         RememberMeToken rememberMeToken = rememberMeTokenRepository.findOne(seriesId);
         if (rememberMeToken != null) {
-            log.info("User " + rememberMeToken.getUser().getEmail() + " logged in using remember-me token");
+            log.info("User " + rememberMeToken.getUser().getEmail() + " logged in using RememberMeToken");
             return new PersistentRememberMeToken(rememberMeToken.getUser().getEmail(), rememberMeToken.getSeries(), rememberMeToken.getValue(), Date.from(rememberMeToken.getDate().toInstant()));
         }
         return null;
     }
 
     @Override
-    public void removeUserTokens(String username) {
-        Set<RememberMeToken> rememberMeTokens = rememberMeTokenRepository.findAllByUser(userRepository.findOneByEmailIgnoringCase(username));
-        for (RememberMeToken rememberMeToken : rememberMeTokens) {
-            rememberMeTokenRepository.delete(rememberMeToken);
-        }
+    public void removeUserTokens(String email) {
+        log.debug("Deleting all RememberMeTokens for user " + email);
+        Long count = rememberMeTokenRepository.deleteByUser(userRepository.findOneByEmailIgnoringCase(email));
+        log.debug(count + " token(s) deleted");
     }
 }
