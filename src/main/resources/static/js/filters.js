90a6d4820e6a5dc48124c0bc4aa2b74217e00033
meonwax <meonwax@meonwax.de>
2018-06-07 23:42:21 +0200

    Translate timezone abbreviation

--- src/main/resources/static/js/filters.js
+++ src/main/resources/static/js/filters.js
@@ -1,9 +1,10 @@
 'use strict';
 
 angular.module('predictrApp')
-  .filter('myDate', function($filter) {
+  .filter('myDate', function($filter, $translate) {
     return function(timestamp) {
-      return $filter('amDateFormat')(timestamp * 1000, 'lll z');
+      let dateString = $filter('amDateFormat')(timestamp * 1000, 'lll z');
+      return timezoneTranslator($translate, dateString);
     };
   })
   .filter('myDateShort', function($filter) {
@@ -11,16 +12,17 @@ angular.module('predictrApp')
       return $filter('amDateFormat')(timestamp * 1000, 'L LT');
     };
   })
-  .filter('myDateLong', function($filter) {
+  .filter('myDateLong', function($filter, $translate) {
     return function(timestamp) {
-      return $filter('amDateFormat')(timestamp * 1000, 'll LTS z');
+      let dateString = $filter('amDateFormat')(timestamp * 1000, 'll LTS z');
+      return timezoneTranslator($translate, dateString);
     };
   })
   .filter('playTime', function() {
     return function(timestamp) {
-      var diffSeconds = Date.now() / 1000 - timestamp;
-      var diffMinutes = Math.floor(diffSeconds / 60);
-      var diffHours = Math.floor(diffMinutes / 60);
+      let diffSeconds = Date.now() / 1000 - timestamp;
+      let diffMinutes = Math.floor(diffSeconds / 60);
+      let diffHours = Math.floor(diffMinutes / 60);
       return diffHours + ':' + ('0' + (diffMinutes - diffHours * 60)).slice(-2) + 'h';
     };
   })
@@ -34,3 +36,12 @@ angular.module('predictrApp')
       return rank ? rank + '.' : '';
     };
   });
+
+let timezoneTranslator = function($translate, dateString) {
+  switch($translate.use()) {
+    case 'de':
+      return dateString.replace(new RegExp("CEST", 'g'), "MESZ");
+    default:
+      return dateString;
+  }
+};
