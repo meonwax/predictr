c918e61ae867de4aca4ad427f8a5e724507b3eb8
meonwax <meonwax@meonwax.de>
2024-05-06 21:15:43 +0200

    Add CSS file for custom rules

--- src/main/java/de/meonwax/predictr/domain/Question.java
+++ src/main/java/de/meonwax/predictr/domain/Question.java
@@ -1,6 +1,7 @@
 package de.meonwax.predictr.domain;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
@@ -8,7 +9,8 @@ import java.time.Instant;
 import java.util.Set;
 
 @Entity
-@Data
+@Getter
+@Setter
 public class Question {
 
     @Id
